04e74a5e21eef6fe4cf3eee028e68f292d61ce8f
mvanne <68028292+mvanne@users.noreply.github.com>
2020-08-28 23:19:46 -0400

    Add project files.

---
+++ HotelApi/Services/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using MyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi.Services
+{
+    public interface IRoomService
+    {
+        Task<Room> GetRoomAsync(Guid id);
+    }
+}
